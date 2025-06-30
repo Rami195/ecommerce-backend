a2099eaa056400e4eb4ba396fc72df76034b4b94
Ramii195 <ramiromartinez3596@gmail.com>
2025-06-30 15:13:51 -0300

    Primer commit - Backend e-commerce

---
+++ src/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { PrismaClient } = require('@prisma/client');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API funcionando 🚀');
+});
+
+// Ejemplo de endpoint de prueba:
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
