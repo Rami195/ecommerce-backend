b299a358ab03149d91af8edd80e14a5267f48d0a
Ramii195 <ramiromartinez3596@gmail.com>
2025-07-01 14:17:22 -0300

    Categoria y avance de articulo

--- src/routes/categoria.routes.js
+++ src/routes/categoria.routes.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 // Importa las funciones del controlador
-const { getCategorias, createCategoria } = require('../controllers/categoria.controller');
+const { getCategorias, createCategoria, updateCategoria, deleteCategoria } = require('../controllers/categoria.controller');
 
 
 
 // Ruta para crear una nueva categoría
 router.post('/', createCategoria);
+router.get('/', getCategorias);
+router.delete('/:id',deleteCategoria);
+router.put('/:id', updateCategoria);
 
 module.exports = router;
