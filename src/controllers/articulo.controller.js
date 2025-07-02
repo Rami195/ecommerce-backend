49fc52f7da97d17d61b8dc3fcc05bdc04982038f
Ramii195 <ramiromartinez3596@gmail.com>
2025-07-02 12:48:17 -0300

    Arreglo prisma y update articulo

--- src/controllers/articulo.controller.js
+++ src/controllers/articulo.controller.js
@@ -1,10 +1,10 @@
 const prisma = require('../prismaClient'); // Importa el cliente de Prisma para interactuar con la base de datos
 
-
+//GET
 exports.getArticulos = async (req, res) => {
   try {
     const articulos = await prisma.articulo.findMany({
-        include: {categorias:true} //incluye las categortias asociadas
+      include: { categorias: true } //incluye las categortias asociadas
     }); // Obtiene todos los artículos de la base de datos
     res.json(articulos); // Envía los artículos como respuesta en formato JSON
   } catch (error) {
@@ -13,23 +13,74 @@ exports.getArticulos = async (req, res) => {
   }
 }
 
-exports.createArticulo = async (req,res)=>{ //ruta del art
-    try{
-    const {nombreArticulo, descripcion, stock, precio, codCategoria}=req.body //lo que envia el cliente
-    const nuevoArt= await prisma.articulo.create({ //nuevo articulo
-        data:{
-            nombreArticulo,
-            descripcion,
-            stock,
-            precio,
-            categorias:{
-                connect: codCategoria.map(id=>({codCategoria:id}))
-            }
+//CREATE
+exports.createArticulo = async (req, res) => { //ruta del art
+  try {
+    const { nombreArticulo, descripcion, stock, precio, codCategoria } = req.body //lo que envia el cliente
+    const nuevoArt = await prisma.articulo.create({ //nuevo articulo
+      data: {
+        nombreArticulo,
+        descripcion,
+        stock,
+        precio,
+        categorias: {
+          connect: codCategoria.map(id => ({ codCategoria: id }))
         }
+      }
     })
     res.status(201).json(nuevoArt) //status 201 es creado correctamente
-    }catch(error){
-        console.error('Error al crear el articulo')
-        res.status(500).json({error: 'Error al crear el articulo'})
+  } catch (error) {
+    console.error('Error al crear el articulo')
+    res.status(500).json({ error: 'Error al crear el articulo' })
+  }
+}
+
+exports.updateArticulo = async (req, res) => {
+  try {
+    const codArticulo = parseInt(req.params.id);
+    const { nombreArticulo, descripcion, stock, precio, categorias } = req.body;
+
+    const existe = await prisma.articulo.findUnique({
+      where: { codArticulo }
+    });
+    if (!existe) {
+      return res.status(404).json({ error: 'Articulo no encontrado' });
     }
+
+    const artActualizado = await prisma.articulo.update({
+      where: { codArticulo },
+      data: {
+        nombreArticulo,
+        descripcion,
+        stock,
+        precio,
+        categorias: {
+          set: categorias.map(id => ({ codCategoria: id }))
+        }
+      }
+    });
+
+    res.status(200).json(artActualizado);
+  } catch (error) {
+    console.error('Error al modificar el articulo:', error);
+    res.status(500).json({ error: 'Error al modificar el articulo' });
+  }
+};
+
+
+//DELETE
+
+exports.deleteArticulo =async (req, res)=>{
+  try{
+    const codArticulo= parseInt(req.params.id)
+    const artEliminado= await prisma.articulo.update({
+      where: {codArticulo},
+      data: {
+        fechaBajaArticulo: new Date()
+      }
+    })
+  }catch(error){
+    console.error('Error al eliminar el articulo:', error);
+    res.status(500).json({ error: 'Error al eliminar el articulo' });
+  }
 }
--- src/routes/articulo.routes.js
+++ src/routes/articulo.routes.js
@@ -1,10 +1,13 @@
 const express = require('express'); //Importa express para crear el router
 const router = express.Router(); //Crea un nuevo router de Express
 
-const { getArticulos, createArticulo } = require('../controllers/articulo.controller'); //Importa los controladores para manejar las rutas
+const { getArticulos, createArticulo,updateArticulo,deleteArticulo } = require('../controllers/articulo.controller'); //Importa los controladores para manejar las rutas
 
 
-router.get('/', getArticulos); //Define la ruta GET para obtener todos los artículos, usando el controlador getArticulos
-router.post('/', createArticulo); //Define la ruta POST para crear un nuevo artículo, usando el
+router.get('/', getArticulos); 
+router.post('/', createArticulo); 
+router.put('/:id',updateArticulo);
+router.delete('/:id',deleteArticulo);
+
 
 module.exports = router; //Exporta el router para que pueda ser utilizado en otros archivos
